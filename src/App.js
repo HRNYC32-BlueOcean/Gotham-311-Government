228e29e15cf107b010e38c4effb126249c359793
Daniel Kang <dkang23@gmail.com>
2020-10-31 12:53:30 -0400

    skeleton for issues page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-
-import React from "react";
+import React from 'react';
 import Button from '@material-ui/core/Button';
-
+import IssuesPage from './components/IssuesPage.jsx';
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
       <>
-        <h1>
-          Hello {name}
-        </h1>
-        <Button variant="contained">this is a material UI button</Button>
+        <h1>Hello {name}</h1>
+        <IssuesPage />
+        <Button variant='contained'>this is a material UI button</Button>
       </>
     );
   }
---
+++ src/components/IssuesPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import IssuesList from './IssuesList';
+const IssuesPage = (props) => {
+  return (
+    <div>
+      {/* <Statuses /> */}
+      <IssuesList
+      // issues={props.}
+      />
+    </div>
+  );
+};
+
+export default IssuesPage;
