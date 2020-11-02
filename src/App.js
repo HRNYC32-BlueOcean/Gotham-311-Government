8e2590e0beca415e70ee9d53b65033a927005be3
Daniel Kang <dkang23@gmail.com>
2020-11-02 13:23:08 -0500

    accordion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import IssuesPage from './components/IssuesPage.jsx';
-import UserData from './components/UserData/UserData.jsx'
+import UserData from './components/UserData/UserData.jsx';
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
       <div>
         <IssuesPage />
-        <UserData/>
+        <UserData />
       </div>
     );
   }
--- src/components/IssuesPage.jsx
+++ src/components/IssuesPage.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
 import IssuesList from './IssuesList';
 const IssuesPage = (props) => {
+  const example = [
+    {
+      description: 'test',
+      id: 'test',
+      reported_count: 'test',
+      task_owner: 'test',
+      username: 'test',
+      user_id: 'test',
+      upvoted_count: 'test',
+      resolution_status: 'test',
+      confirm_resolved_count: 'test',
+      date_issued: 'test',
+      date_marked_in_progress: 'test',
+      date_marked_resolved: 'test',
+    },
+    {
+      description: 'test2',
+      id: 'test2',
+      reported_count: 'test2',
+      task_owner: 'test2',
+      username: 'test2',
+      user_id: 'test2',
+      upvoted_count: 'test2',
+      resolution_status: 'test2',
+      confirm_resolved_count: 'test2',
+      date_issued: 'test2',
+      date_marked_in_progress: 'test2',
+      date_marked_resolved: 'test2',
+    },
+  ];
+
   return (
     <div>
       {/* <Statuses /> */}
-      <IssuesList
-      // issues={props.}
-      />
+      <IssuesList issues={example} />
     </div>
   );
 };
