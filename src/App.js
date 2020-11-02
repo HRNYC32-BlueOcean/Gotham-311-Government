925dcc6a5298f8d6f73ad7cadf607e3d60561471
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-02 14:48:04 -0500

    Render employee dashboard component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-
 import React from "react";
 import Button from '@material-ui/core/Button';
+import EmployeeDashboard from './components/EmployeeDashboard/EmployeeDashboard.js';
 
 class App extends React.Component {
+
   render() {
     const { name } = this.props;
     return (
       <>
-        <h1>
-          Hello {name}
-        </h1>
-        <Button variant="contained">this is a material UI button</Button>
+        <EmployeeDashboard/>
       </>
     );
   }
