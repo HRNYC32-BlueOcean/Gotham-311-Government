d61d8c652e96d524b8bd833c9ba0f72304589e68
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-03 12:54:14 -0500

    Reorganize src folder and implement react-router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const EXPRESS = require('express');
 const PATH = require('path');
+const ROUTER = EXPRESS.Router();
 
 const PORT = process.env.PORT || 3005;
 
@@ -9,6 +10,14 @@ APP.use(EXPRESS.static(PATH.join(__dirname, './dist')));
 
 APP.get('/', (req, res) => {
   res.status(200).send('index');
+});
+
+APP.get('/issues', (req, res) => {
+  res.redirect('/');
+});
+
+APP.get('/users', (req, res) => {
+  res.redirect('/');
 });
 
 APP.listen(PORT, () => {
