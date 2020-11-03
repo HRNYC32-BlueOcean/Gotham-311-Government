0aae93dec9a06fb9e8f4a276640a340be8dedb91
Daniel Kang <dkang23@gmail.com>
2020-11-03 12:49:09 -0500

    issues page update

--- src/components/IssuesPage.jsx
+++ src/components/IssuesPage.jsx
@@ -1,43 +1,40 @@
 import React, { useState } from 'react';
 import IssuesList from './IssuesList';
-const IssuesPage = (props) => {
-  const example = [
-    {
-      description: 'test',
-      id: 'test',
-      reported_count: 'test',
-      task_owner: 'test',
-      username: 'test',
-      user_id: 'test',
-      upvoted_count: 'test',
-      resolution_status: 'test',
-      confirm_resolved_count: 'test',
-      date_issued: 'test',
-      date_marked_in_progress: 'test',
-      date_marked_resolved: 'test',
-    },
-    {
-      description: 'test2',
-      id: 'test2',
-      reported_count: 'test2',
-      task_owner: 'test2',
-      username: 'test2',
-      user_id: 'test2',
-      upvoted_count: 'test2',
-      resolution_status: 'test2',
-      confirm_resolved_count: 'test2',
-      date_issued: 'test2',
-      date_marked_in_progress: 'test2',
-      date_marked_resolved: 'test2',
-    },
-  ];
-
-  return (
-    <div>
-      {/* <Statuses /> */}
-      <IssuesList issues={example} />
-    </div>
-  );
-};
+const example = [
+  {
+    description: 'test',
+    id: 'test',
+    reported_count: 'test',
+    task_owner: 'test',
+    username: 'test',
+    user_id: 'test',
+    upvoted_count: 'test',
+    resolution_status: 'test',
+    confirm_resolved_count: 'test',
+    date_issued: 'test',
+    date_marked_in_progress: 'test',
+    date_marked_resolved: 'test',
+  },
+  {
+    description: 'test2',
+    id: 'test2',
+    reported_count: 'test2',
+    task_owner: 'test2',
+    username: 'test2',
+    user_id: 'test2',
+    upvoted_count: 'test2',
+    resolution_status: 'test2',
+    confirm_resolved_count: 'test2',
+    date_issued: 'test2',
+    date_marked_in_progress: 'test2',
+    date_marked_resolved: 'test2',
+  },
+];
+const IssuesPage = (props) => (
+  <div>
+    {/* <Statuses /> */}
+    <IssuesList issues={example} />
+  </div>
+);
 
 export default IssuesPage;
