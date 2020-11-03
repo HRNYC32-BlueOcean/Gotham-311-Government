f79d9a3c8e53824ace29fb887f257c3ba6db14f5
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-03 10:19:50 -0500

    Remove file extensions from imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import EmployeeDashboard from './components/EmployeeDashboard/EmployeeDashboard.jsx';
-import IssuesPage from './components/IssuesPage.jsx';
-import UserData from './components/UserData/UserData.jsx';
+import EmployeeDashboard from './components/EmployeeDashboard/EmployeeDashboard';
+import IssuesPage from './components/IssuesPage';
+import UserData from './components/UserData/UserData';
 
 class App extends React.Component {
   render() {
