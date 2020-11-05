765d3a4fde42acebd6e3a7c53694137bd1152d06
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-04 19:00:12 -0500

    Add login route

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const EXPRESS = require('express');
+const ROUTER = EXPRESS.Router();
 const PATH = require('path');
 const PORT = process.env.PORT || 3005;
 
 const APP = EXPRESS();
 
 APP.use(EXPRESS.static(PATH.join(__dirname, 'dist')));
+const distPath = PATH.join(__dirname, 'dist');
 
-APP.get('/', (req, res) => {
+ROUTER.get('/', (req, res) => {
   res.status(200).send('index')
 });
 
@@ -17,6 +19,10 @@ APP.get('/issues', (req, res) => {
 
 APP.get('/users', (req, res) => {
   res.redirect('/');
+});
+
+APP.get('/login', (req, res) => {
+  res.sendFile(distPath + '/login.html');
 });
 
 APP.listen(PORT, () => {
